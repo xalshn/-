6aeee0019649cf61e2090b61ca98969b16517225
xalshn <alchenbayavdimash@gmail.com>
2024-11-25 17:57:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+// ///1
+// let season = +prompt("1-12ге дейын сан енгыз")
+
+// switch(season){
+//     case 3:
+//         alert("Ай 3 spring")
+//         break;
+//     case 4:
+//         alert("Ай 4 spring")
+//         break;
+//     case 5:
+//         alert("Ай 5 spring")
+//         break;
+//     case 6:
+//         alert("Ай 6 summer")
+//         break;
+//     case 7:
+//         alert("Ай 7 summer")
+//         break;
+//     case 8:
+//         alert("Ай 8 summer")
+//         break;
+//     case 9:
+//         alert("Ай 9 fall")
+//         break;
+//     case 10:
+//         alert("Ай 10 fall")
+//         break;
+//     case 11:
+//         alert("Ай 11 fall")
+//         break;
+//     case 12:
+//         alert("Ай 12 winter")
+//         break;
+//     case 1:
+//         alert("Ай 1 winter")
+//         break;
+//     case 2:
+//         alert("Ай 2 winter")
+//         break;
+//     default:
+//         alert("Sen talapqa sai san engizbedin. Qaita ter")
+//         break;
+// }
+
+
+
+
+
+
+///2
+let a = +prompt("a sanyn engiz:")
+let b = +prompt("b sanyn engiz:")
+
+if(a > b){
+    alert(a + " саны " + b + "санынан улкен" + '\n' + "Осы сандардын орташа маны :" + ((a+b)/2))
+}
+else if(a < b){
+    alert(b + " саны " + a + "санынан улкен" + '\n' + "Осы сандардың орташа маны:" + ((a+b)/2))
+}
+else if(a == b){
+    alert(b + " жане " + a + "сандары тен" + '\n' + "Осы сандардың орташа маны:" + ((a+b)/2))
+}
+else{
+    alert("Sen talapqa sai engizbedin")
+}
+
+
+
+
+///3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
